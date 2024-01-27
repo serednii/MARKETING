f3815059860acbeac0cb8dfbf9f2ec263b3feff6
Serednia Anastasia <anastastasiaseredna.gmail.com>
2024-01-27 13:39:34 +0100

    add box-sizing: border-box

--- js/main.js
+++ js/main.js
@@ -18,10 +18,8 @@ $(function(){
                     arrows: false
                 }
               },
-       
             ]
           });
-
 
     $('.menu__btn, .menu a').on('click', function(){
         $('.header__top-inner').toggleClass('header__top-inner--active');
